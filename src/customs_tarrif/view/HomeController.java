256898c9b98e687db88e74862e1f8cdf649349f4
Mahmud Raju <mahmudkuet11@gmail.com>
2015-05-16 22:51:05 +0600

    initial commit

---
+++ src/customs_tarrif/view/HomeController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customs_tarrif.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ * FXML Controller class
+ *
+ * @author mohar
+ */
+public class HomeController implements Initializable {
+    @FXML
+    private MenuItem add_new_item;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onAddNewItemClick(ActionEvent event) {
+
+    }
+    
+}
